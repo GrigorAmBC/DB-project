7e095ce46f8cc3e9b24f6878059eca94b201b598
Grigor <g.abgaryan@g.nsu.ru>
2021-02-13 21:44:25 +0700

    fixup

--- DbMainProject/grails-app/init/dbmainproject/BootStrap.groovy
+++ DbMainProject/grails-app/init/dbmainproject/BootStrap.groovy
@@ -1,9 +1,7 @@
 package dbmainproject
 
 import ru.nsu.fit.grigor.dbproject.domain.Department
-import ru.nsu.fit.grigor.dbproject.domain.DepartmentService
 import ru.nsu.fit.grigor.dbproject.domain.Team
-import ru.nsu.fit.grigor.dbproject.domain.TeamService
 
 class BootStrap {
 
@@ -13,10 +11,10 @@ class BootStrap {
         Department d3 = new Department(name: "HR").save()
         Department d4 = new Department(name: "CHOP").save()
 
-        new Team(department: d2, createdAt: getDate(2015 - 1900, 2, 2), budget: 10000, name: "team2", isWorking: true).save()
-        new Team(department: d1, createdAt: getDate(2020 - 1900, 11, 3), budget: 41234, name: "team3", isWorking: true).save()
-        new Team(department: d3, createdAt: getDate(2005 - 1900, 12, 15), budget: 29201, name: "team4", isWorking: false).save()
-        new Team(department: d4, createdAt: getDate(2000 - 1900, 7, 21), budget: 120, name: "team5", isWorking: false).save()
+        new Team(department: d2, createdAt: getDate(2015, 2, 2), budget: 10000, name: "team2", isWorking: true).save()
+        new Team(department: d1, createdAt: getDate(2020, 11, 3), budget: 41234, name: "team3", isWorking: true).save()
+        new Team(department: d3, createdAt: getDate(2005, 12, 15), budget: 29201, name: "team4", isWorking: false).save()
+        new Team(department: d4, createdAt: getDate(2000, 7, 21), budget: 120, name: "team5", isWorking: false).save()
     }
     def destroy = {
     }
